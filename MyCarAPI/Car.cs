a83ad6177df1c2e2de938307db58fcc97cae5f92
NataliaMilgevskaya <milgevskaya.n@gmail.com>
2023-04-10 17:43:20 +0300

    Task 1. Create Asp.net Web API application. Add new controller Cars Add method get that should return a list of cars P.S. Model cars you can describe as you wish, but it should contain at least 4 properties

---
+++ MyCarAPI/Car.cs
@@ -0,0 +1,13 @@
+﻿namespace MyCarAPI
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        public string PhotoLink { get; set; }
+
+    }
+}
